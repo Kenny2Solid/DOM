fa0f69a845d5b5b07ca7e8bf1e4490dc2628069d
Kendrick Torrance <torraken000@warren.k12.in.us>
2025-03-06 07:41:54 -0500

    diddy

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+let count = 0;
+
+const counterDisplay = document.getElementById('counter-display');
+const incrementBtn = document.getElementById('increment-btn');
+const decrementBtn = document.getElementById('decrement-btn');
+
+incrementBtn.addEventListener('click', () => {
+    count++;
+    counterDisplay.textContent = count;
+});
+
+decrementBtn.addEventListener('click', () => {
+    count--;
+    counterDisplay.textContent = count;
+});
